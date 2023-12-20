d018f7fc88dff300294998765dcab05d8cd38cf0
Thelaxman <gusainlakshman@gmail.com>
2023-12-20 12:04:09 -0700

    Before deployment

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import "./App.css";
+import React, { useEffect } from "react";
+import Header from "./Header";
+import Home from "./Home";
+import Checkout from "./Checkout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import { auth } from "./firebase";
+
+import { useStateValue } from "./Stateprovider";
+
+function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log(authUser);
+
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,66 @@
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import { ShoppingBasket } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { useStateValue } from "./Stateprovider";
+import { auth } from "./firebase";
+
+function Header() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  const handleauthentication = () => {
+    if (user) {
+      auth.signOut();
+    }
+  };
+
+  return (
+    <div className="header">
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="https://companieslogo.com/img/orig/AMZN_BIG.D-8fb0be81.png?t=1632523695"
+          alt="logo"
+        />
+      </Link>
+
+      <div className="header_search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header_searchIcon" />
+      </div>
+
+      <div className="header_nav">
+        <div className="header_option" onClick={handleauthentication}>
+          <span className="header_navoptionOne">
+            Hello {!user ? "Guest" : user.email}
+          </span>
+          <Link to={!user && "./login"}>
+            {" "}
+            <span className="header_navoptionTwo">
+              {user ? "SignOut" : "SignIn"}
+            </span>
+          </Link>
+        </div>
+        <div className="header_option">
+          <span className="header_navoptionOne">Returns</span>
+          <span className="header_navoptionTwo">& Orders</span>
+        </div>
+        <div className="header_option">
+          <span className="header_navoptionOne">Your</span>
+          <span className="header_navoptionTwo">Prime</span>
+        </div>
+
+        <Link to="/checkout">
+          <div className="header_basketIcon">
+            <ShoppingBasket />
+            <span className="header_navoptionTwo header_basketCount">
+              {basket.length}
+            </span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
